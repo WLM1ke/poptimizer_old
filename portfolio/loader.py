03cceda4d32ea12a2045a47db88c9e91bb822cf4
WLM1ke <wlmike@gmail.com>
2018-02-24 20:06:59 +0300

    Реализована загрузка данных по нескольким тикерам - close #4
    Переделана логика формирования данных:
    Ранее данные выбирались по наименованию столбцов
    Сейчас формируется два DataFrame с данными описания и котировками
    Потом из них выбираются нужные столбцы
    DataFrame склеиваются в один

--- portfolio/loader.py
+++ portfolio/loader.py
@@ -4,32 +4,57 @@ from urllib import request
 import pandas as pd
 
 
-def security_info(ticker):
-    if not isinstance(ticker, str):
-        raise ValueError('Тикер должен быть строкой')
+def security_info(tickers):
+    """
+    Возвращает краткое наименование, размер лота и последнюю цену
 
-    url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{ticker}.json'
-    with request.urlopen(url.format(ticker=ticker)) as response:
+    Parameters
+    ----------
+    tickers : str or list of str
+        Тикер или список тикеров
+
+    Returns
+    -------
+    pandas.DataFrame
+        В строках тикеры большими буквами
+        В столбцах краткое наименование, размер лота и последняя цена
+    """
+
+    if isinstance(tickers, str):
+        tickers = [tickers.upper()]
+    elif isinstance(tickers, list):
+        tickers = [i.upper() for i in tickers]
+    else:
+        raise ValueError('Тикер должен быть строкой или списком строк')
+
+    url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json?securities={tickers}'
+    with request.urlopen(url.format(tickers=','.join(tickers))) as response:
         data = json.load(response)
 
-    if len(data) != 3:
-        raise ValueError('В словаре данных должно быть три поля')
-    elif len(data['securities']['data']) == 0:
-        raise ValueError('Неверный тикер')
+    # Полный ответ сервера - словарь с тремя ключами
+    # По ключу securities - словарь с описанием инструментов
+    # По ключу marketdata - словарь с последними котировками
+    # В кажом из вложеных словарей есть ключи columns и data с масивами описания колонок и данными
+    # В массиве данных содержатся массивы для каждого запрошенного тикера
 
-    result = pd.Series(name=ticker)
-    data_fields = [['Name', 'securities', 'SHORTNAME'],
-                   ['Lot size', 'securities', 'LOTSIZE'],
-                   ['Last price', 'marketdata', 'LAST']]
-    for index, block, field in data_fields:
-        position = data[block]['columns'].index(field)
-        if len(data[block]['data']) != 1:
-            raise ValueError('В списке должен быть один элемент')
-        result[index] = data[block]['data'][0][position]
+    if len(data) != 3:
+        print('Сервер вернул словарь с ', len(data), ' ключами')
+        raise ValueError('Неверное число ключей в словаре')
+    elif len(data['securities']['data']) != len(tickers):
+        raise ValueError('Количество тикеров в ответе не соответсвует запросу - возможно ошибка в написании')
+    elif len(data['marketdata']['data']) != len(tickers):
+        raise ValueError('Количество тикеров в ответе не соответсвует запросу - возможно ошибка в написании')
+
+    securities = pd.DataFrame(data=data['securities']['data'], columns=data['securities']['columns'])
+    marketdata = pd.DataFrame(data=data['marketdata']['data'], columns=data['marketdata']['columns'])
+
+    securities = securities.set_index('SECID')[['SHORTNAME', 'LOTSIZE']]
+    marketdata = marketdata.set_index('SECID')['LAST']
+    result = pd.concat([securities, marketdata], axis=1)
 
     return result
 
 
 if __name__ == '__main__':
-    data = security_info('GAZP')
+    data = security_info(['aKRN', 'gAZP', 'LKOH'])
     print(data)
