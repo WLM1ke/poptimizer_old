b3895e07a1c4308eecfe92e778d944d8abf51821
WLM1ke <wlmike@gmail.com>
2018-03-03 20:54:06 +0300

    Тесты на начальную дату индекса и загрузку более 100 строк данных

--- src/portfolio/tests/test_loader_index.py
+++ src/portfolio/tests/test_loader_index.py
@@ -2,11 +2,18 @@ import datetime
 
 import pandas as pd
 
-from ..loader_index import get_index_history
+from ..loader_index import get_index_history, get_raw_json
 
 
 def test_get_index_history():
-    df = get_index_history(datetime.date(2018, 2, 20))
+    df = get_index_history(datetime.date(2017, 10, 2))
     assert isinstance(df, pd.DataFrame)
-    assert df.index[0] == '2018-02-20'
+    assert df.index[0] == '2017-10-02'
+    assert df.shape[0] > 100
     assert df.loc['2018-03-02', 'CLOSE'] == 3273.16
+
+
+def test_get_raw_json():
+    data = get_raw_json(None, 0)
+    index = data['history']['columns'].index('TRADEDATE')
+    assert data['history']['data'][0][index] == '2003-02-26'
