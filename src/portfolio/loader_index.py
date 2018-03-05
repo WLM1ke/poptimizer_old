e5510b8d3dce322e716437700b3393849f04152d
=epogrebnyak <e.pogrebnyak@gmail.com>
2018-03-05 21:28:06 +0300

    uses an iterator

--- src/portfolio/loader_index.py
+++ src/portfolio/loader_index.py
@@ -5,21 +5,21 @@ import datetime
 import pandas as pd
 import requests
 
-#FIXME: выглядит таким образом, что block_position - обязательный параметр,
-#       a start_date - необзятальный, 
-#       если так, то они должны в обратном порядке идти
 def make_url(start_date=None, block_position=0):
     """
-    Возвращает url для получения очередного блока данных по индексу полной доходности MOEX.
+    Возвращает url для получения очередного блока данных по индексу 
+    полной доходности MOEX.
 
     Parameters
     ----------
-    start_date : date.time
-        Начальная дата в рамках запроса. Если тип данных отличается от date.time - данные запрашиваются с начала
-        имеющейся на серевере ISS истории котировок.
+    start_date : date.time or None
+        Начальная дата котировок. Если предоставлено значение None 
+        - данные запрашиваются с начала имеющейся на сервере ISS 
+        истории котировок.
     block_position : int
-        Позиция курсора, начиная с которой необходимо получить очередной блок данных. При большом запросе сервер ISS
-        возвращает данные блоками обычно по 100 значений. Нумерация позиций в ответе идет с 0.
+        Позиция курсора, начиная с которой необходимо получить очередной блок 
+        данных. При большом запросе сервер ISS возвращает данные блоками обычно 
+        по 100 значений. Нумерация позиций в ответе идет с 0.
 
     Returns
     -------
@@ -31,9 +31,6 @@ def make_url(start_date=None, block_position=0):
     if start_date:        
         if not isinstance(start_date, datetime.date):
             raise TypeError(start_date)
-        # start_date желательно приводить в нужный формат явным образом 
-        # https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
-        # в прошлом коде формат совпадает "наугад" по умолчаниям
         url = url + f'&from={start_date:%Y-%m-%d}'
     return url
 
@@ -55,6 +52,8 @@ class TotalReturn:
 
     def validate(self, block_position):
         if len(self) == 0 and block_position == 0:
+            # FIXME: мы точно такую ошибку ждем? 'в написании' чего?
+            #        тут же нет тикеторв? ввиду написание url?
             raise ValueError('Пустой ответ - возможно ошибка в написании')
         
     def __len__(self):
@@ -75,32 +74,44 @@ class TotalReturn:
     def dataframe(self):
         df = pd.DataFrame(data=self.values, columns=self.columns)
         return df[['TRADEDATE', 'CLOSE']]
-        
+  
+
+def yield_index_blocks(start_date):
+    """Yield pandas dataframes until response length is exhausted."""
+    block_position = 0
+    current_response = True
+    while current_response:
+        current_response = TotalReturn(start_date, block_position)
+        block_position += len(current_response)
+        yield current_response.dataframe
+           
     
-def get_index_history(start_date=None):
+def get_index_history(start_date):
     """
     Возвращает историю котировок индекса полной доходности с учетом российских налогов.
 
     Parameters
     ----------
     start_date : datetime.date
-        Начальная дата котировок. Если тип данных отличается от date.time - данные запрашиваются с начала
-        имеющейся на серевере ISS истории котировок.
-
+        Начальная дата котировок. 
+        
     Returns
     -------
     pandas.DataFrame
         В строках даты торгов.
         В столбцах цена закрытия индекса полной доходности.
     """
-    block_position = 0
-    current_response = True
-    result = pd.DataFrame()
-    while current_response:
-        current_response = TotalReturn(start_date, block_position)
-        block_position += len(current_response)
-        result = pd.concat([result, current_response.dataframe])
+    result = pd.concat(yield_index_blocks(start_date))
     return result.set_index('TRADEDATE')
+
+def get_index_history_from_start():
+    """
+    Возвращает историю котировок индекса полной доходности с учетом российских налогов.
+    Данные запрашиваются с начала имеющейся на сервере ISS истории котировок.
+    
+    Предполгаемая дата начала котировок - 2003-02-26.    
+    """
+    return get_index_history(start_date=None)
 
 
 if __name__ == '__main__':
--- src/portfolio/tests/test_loader_index.py
+++ src/portfolio/tests/test_loader_index.py
@@ -6,9 +6,9 @@ from ..loader_index import make_url, get_index_history, get_raw_json
 
 
 def test_make_url():
-    url = make_url(datetime.date(2017, 10, 1), 5)
-    assert url = ('http://iss.moex.com/iss/history/engines/stock/markets/index/'
-                  'boards/RTSI/securities/MCFTRR.json?start=5&from=2017-10-01')    
+    url = make_url(datetime.date(2017, 10, 1), 50)
+    assert url ==  ('http://iss.moex.com/iss/history/engines/stock/markets/index/'
+                    'boards/RTSI/securities/MCFTRR.json?start=50&from=2017-10-01')    
     
 def test_get_index_history():
     df = get_index_history(datetime.date(2017, 10, 2))
@@ -18,8 +18,8 @@ def test_get_index_history():
     assert df.loc['2018-03-02', 'CLOSE'] == 3273.16
 
 
-def test_get_raw_json():
-    data = get_raw_json(None, 0)
+def test_get_raw_json_works_on_None_start_date():
+    data = get_raw_json(start_date=None, block_position=0)
     index = data['history']['columns'].index('TRADEDATE')
     assert data['history']['data'][0][index] == '2003-02-26'
     
