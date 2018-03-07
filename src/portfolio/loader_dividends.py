8a7404a0b554b87f5d1515b9fb99fce4d7c56cd8
WLM1ke <wlmike@gmail.com>
2018-03-07 18:33:18 +0300

    Набросок из #6

--- src/portfolio/loader_dividends.py
+++ src/portfolio/loader_dividends.py
@@ -1,15 +1,51 @@
 """Dividends history downloader and parser."""
 
-import bs4
-import requests
+import urllib.request
+from pathlib import Path
 
-# Посмотреть - http://html.python-requests.org
-# При необходимости поменять requirements
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
-url = 'https://www.dohod.ru/ik/analytics/dividend/rtkmp'
-page = requests.get(url).content
-soup = bs4.BeautifulSoup(page, "html5lib")
-soup.find_all('table')
-i = soup.find(name='p', attrs='table-title', string='Все выплаты').next_sibling
-print(i)
+def get_text(url):
+    # *requests* fails on SSL, using *urllib.request*
+    with urllib.request.urlopen(url) as response:
+        return response.read().decode("utf-8")
+
+
+def yield_rows(table: str):
+    for html_row in table.find_all('tr'):
+        row = [column.text.strip() for column in html_row.find_all('td')]
+        try:
+            yield dict(DATE=pd.to_datetime(row[0]), DIVIDEND=row[2])
+        except (IndexError, ValueError) as e:
+            print("Not parsed:", row)
+
+
+def make_dataframe(table: str):
+    df = pd.DataFrame(columns=('DATE', 'DIVIDEND'))
+    for row in yield_rows(table):
+        df = df.append(row, ignore_index=True)
+    return (df.set_index('DATE'))
+
+
+if __name__ == '__main__':
+    url = 'http://www.dohod.ru/ik/analytics/dividend/rtkmp'
+    # dirty cache
+    cache = Path('temp.txt')
+    if cache.exists():
+        html = cache.read_text()
+    else:
+        html = get_text(url)
+        cache.write_text(html)
+        # identify table
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find_all('table')[2]
+    df = make_dataframe(table)
+    print(df)
+
+header = soup.find(name='th', text='Дата закрытия реестра').parent
+
+for n, column in enumerate(header.findall(name='th')):
+    if column.string in {'Дата закрытия реестра', 'Дивиденд (руб.)'}:
+        print(n)
