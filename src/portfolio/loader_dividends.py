f4b071db416db86748a76144277e0ed07b4a03fa
WLM1ke <wlmike@gmail.com>
2018-03-07 08:41:03 +0300

    Добавлен файл загрузчика дивидендов - пока разбираюсь с bs4
    bs4 добавлена в список необходимых библиотек

---
+++ src/portfolio/loader_dividends.py
@@ -0,0 +1,11 @@
+"""Dividends history downloader and parser."""
+
+import bs4
+import requests
+
+url = 'https://www.dohod.ru/ik/analytics/dividend/rtkmp'
+page = requests.get(url).content
+soup = bs4.BeautifulSoup(page, "html5lib")
+soup.find_all('table')
+i = soup.find(name='p', attrs='table-title', string='Все выплаты').next_sibling
+print(i)
