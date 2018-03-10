bc4d47d0d3d9baf5b964be6126120424eeeade07
=epogrebnyak <e.pogrebnyak@gmail.com>
2018-03-10 21:37:06 +0300

    some function changes

--- src/portfolio/download/dividends.py
+++ src/portfolio/download/dividends.py
@@ -1,8 +1,9 @@
 """Download and transform dividends data to pandas DataFrames.
 
-    Single ticker close dates and dividends:
+   Single ticker close dates and dividends:
 
-        get_dividends(ticker)
+       get_dividends(ticker)
+
 """
 
 import urllib.error
@@ -11,23 +12,20 @@ import urllib.request
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# Номер таблицы с дивидендами
+# номер таблицы с дивидендами в документе
 TABLE_INDEX = 2
-# Позиции, наименования ключевых столбцов и их имя в DataFrame
+# позиции, наименования ключевых столбцов и их имя в DataFrame
 HEADER = [(0, 'Дата закрытия реестра'),
           (2, 'Дивиденд (руб.)')]
-
-
-def make_url(ticker):
-    # Обычно тикеры пишутся всеми большими буквами, но в url они должны быть маленькими
-    return f'http://www.dohod.ru/ik/analytics/dividend/{ticker.lower()}'
 
 
 def get_html(url):
     try:
         # *requests* fails on SSL, using *urllib.request*
         with urllib.request.urlopen(url) as response:
-            return BeautifulSoup(response.read(), 'lxml')
+            # EP: это плохо тестируется и перескакивает между уровнями
+            # return BeautifulSoup(response.read(), 'lxml')
+            return response.read()
     except urllib.error.HTTPError as error:
         if error.code == 404:
             raise urllib.error.URLError(f'Неверный url: {url}')
@@ -35,14 +33,24 @@ def get_html(url):
             raise error
 
 
-def validate_table_header(header):
-    header_names = [column.string for column in header.find_all(name='th')]
-    if any(header_names[i] != name for i, name in HEADER):
-        raise ValueError('Некоректные заголовки таблицы дивидендов.')
+def make_url(ticker):
+    # Обычно тикеры пишутся всеми большими буквами, но в url они должны быть маленькими
+    ticker = ticker.lower()
+    return f'http://www.dohod.ru/ik/analytics/dividend/{ticker}'
+
+
+def pick_table(html: str, n: int=TABLE_INDEX):
+    soup = BeautifulSoup(html, 'lxml')
+    return soup.find_all('table')[n]
 
 
 def parse_table_rows(html_table):
-    # Строки с прогнозом имеют class = forecast, а у заголовка и факта - класс отсутсвует
+    # Строки с прогнозом имеют class = forecast, а у заголовка и факта 
+    # - класс отсутсвует
+    def validate_table_header(header):
+        header_names = [column.string for column in header.find_all(name='th')]
+        if any(header_names[i] != name for i, name in HEADER):
+            raise ValueError('Некоректные заголовки таблицы дивидендов.')
     rows = html_table.find_all(name='tr', class_=None)
     validate_table_header(rows[0])
     data = []
@@ -61,11 +69,14 @@ def make_df(parsed_rows):
 
 def get_dividends(ticker):
     url = make_url(ticker)
-    html = get_html(url)
-    html_table = html.find_all('table')[TABLE_INDEX]
-    parsed_rows = parse_table_rows(html_table)
+    doc = get_html(url)
+    table = pick_table(doc)
+    parsed_rows = parse_table_rows(table)
     return make_df(parsed_rows)
 
 
 if __name__ == '__main__':
     print(get_dividends('CHMF'))
+    # ВОПРОС: как эти данные дальше используются? склиеваются для одного 
+    # эмитента разные показатели? или дивиденды по разным эмитентам?
+    
