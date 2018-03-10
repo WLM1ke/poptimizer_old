9234701662ef68ad27603793e61cd91a521021a7
=epogrebnyak <e.pogrebnyak@gmail.com>
2018-03-11 00:05:48 +0300

    RowParser class

--- src/portfolio/download/dividends.py
+++ src/portfolio/download/dividends.py
@@ -14,9 +14,11 @@ from bs4 import BeautifulSoup
 
 # номер таблицы с дивидендами в документе
 TABLE_INDEX = 2
-# позиции, наименования ключевых столбцов и их имя в DataFrame
-HEADER = [(0, 'Дата закрытия реестра'),
-          (2, 'Дивиденд (руб.)')]
+# позиции и наименования ключевых столбцов 
+TH_DATE = 'Дата закрытия реестра'
+TH_VALUE = 'Дивиденд (руб.)'
+DATE_COLN = 0
+VALUE_COLN = 2
 
 
 def get_html(url):
@@ -25,7 +27,7 @@ def get_html(url):
         with urllib.request.urlopen(url) as response:
             # EP: это плохо тестируется и перескакивает между уровнями
             # return BeautifulSoup(response.read(), 'lxml')
-            return response.read()
+            return response.read().decode('utf-8')
     except urllib.error.HTTPError as error:
         if error.code == 404:
             raise urllib.error.URLError(f'Неверный url: {url}')
@@ -33,7 +35,7 @@ def get_html(url):
             raise error
 
 
-def make_url(ticker):
+def make_url(ticker: str):
     # Обычно тикеры пишутся всеми большими буквами, но в url они должны быть маленькими
     ticker = ticker.lower()
     return f'http://www.dohod.ru/ik/analytics/dividend/{ticker}'
@@ -44,21 +46,31 @@ def pick_table(html: str, n: int=TABLE_INDEX):
     return soup.find_all('table')[n]
 
 
-def parse_table_rows(html_table):
+class RowParser:
+    def __init__(self, row, tag='td'):
+        self.columns=[column.string for column in row.find_all(tag)]
+    
+    @property
+    def date(self):
+        return self.columns[DATE_COLN] #0
+    
+    @property    
+    def value(self):     
+        return self.columns[VALUE_COLN] #2
+        
+
+def parse_table_rows(table):
+    def validate_table_header(header):
+        r = RowParser(header, 'th')
+        if r.date != TH_DATE or r.value != TH_VALUE:
+            raise ValueError('Некоректные заголовки таблицы дивидендов.')
     # Строки с прогнозом имеют class = forecast, а у заголовка и факта 
     # - класс отсутсвует
-    def validate_table_header(header):
-        header_names = [column.string for column in header.find_all(name='th')]
-        if any(header_names[i] != name for i, name in HEADER):
-            raise ValueError('Некоректные заголовки таблицы дивидендов.')
-    rows = html_table.find_all(name='tr', class_=None)
+    rows = table.find_all(name='tr', class_=None)
     validate_table_header(rows[0])
-    data = []
     for row in rows[1:]:
-        row = [column.string for column in row.find_all('td')]
-        data.append([pd.to_datetime(row[HEADER[0][0]]),
-                     pd.to_numeric(row[HEADER[1][0]])])
-    return data
+        r = RowParser(row)
+        yield pd.to_datetime(r.date), pd.to_numeric(r.value)
 
 
 def make_df(parsed_rows):
@@ -79,4 +91,10 @@ if __name__ == '__main__':
     print(get_dividends('CHMF'))
     # ВОПРОС: как эти данные дальше используются? склиеваются для одного 
     # эмитента разные показатели? или дивиденды по разным эмитентам?
-    
+    # если последнее - то в названии колонки надо выводить тикер 
+    ticker = 'CHMF'
+    url = make_url(ticker)
+    doc = get_html(url)
+    table = pick_table(doc)
+    parsed_rows = parse_table_rows(table)
+    df = make_df(parsed_rows)    
