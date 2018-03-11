a01e2563664fe8c7919fcb6e859a440600269e1a
WLM1ke <wlmike@gmail.com>
2018-03-11 18:14:43 +0300

    Тест переписан с использованием fixture

--- src/portfolio/getter/test/test_history.py
+++ src/portfolio/getter/test/test_history.py
@@ -1,15 +1,32 @@
 import pandas as pd
+import pytest
 
 from portfolio.getter.history import get_quotes_history
 
 
-def test_get_quotes_history():
-    df = get_quotes_history('MSTT')
-    assert isinstance(df, pd.DataFrame)
-    assert len(df.columns) == 2
-    assert df.index.is_monotonic_increasing
-    assert df.index.is_unique
-    assert df.index[0] == pd.to_datetime('2010-11-03')
-    assert df.shape[0] > 100
-    assert df.loc['2018-03-09', 'CLOSE'] == 148.8
-    assert df.loc['2018-03-09', 'VOLUME'] == 2960
+@pytest.fixture(scope='class')
+def get_df():
+    return get_quotes_history('MSTT')
+
+
+class TestGetQuotesHistory:
+    def test_df_type(self, get_df):
+        assert isinstance(get_df, pd.DataFrame)
+
+    def test_df_columns_number(self, get_df):
+        assert len(get_df.columns) == 2
+
+    def test_df_index_monotonic(self, get_df):
+        assert get_df.index.is_monotonic_increasing
+
+    def test_df_is_unique(self, get_df):
+        assert get_df.index.is_unique
+
+    def test_beginning_date(self, get_df):
+        assert get_df.index[0] == pd.to_datetime('2010-11-03')
+
+    def test_load_long_data(self, get_df):
+        assert get_df.shape[0] > 100
+
+    def test_checkpoint(self, get_df):
+        assert get_df.loc['2018-03-09', 'CLOSE'] == 148.8 and get_df.loc['2018-03-09', 'VOLUME'] == 2960
