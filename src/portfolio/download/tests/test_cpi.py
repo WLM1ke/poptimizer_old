2efde292c4f445b75e1c8c5272592880e55016ef
WLM1ke <wlmike@gmail.com>
2018-03-12 20:05:11 +0300

    Переделаны тесты для CPI

--- src/portfolio/download/tests/test_cpi.py
+++ src/portfolio/download/tests/test_cpi.py
@@ -1,7 +1,12 @@
+import pytest
+
 from portfolio.download.cpi import get_monthly_cpi
 
-    
-def test_parse_local_dataframe_cpi():
+CHECK_POINTS = [('1991-01-31', 1.0620),
+                ('2018-01-31', 1.0031)]
+
+
+@pytest.mark.parametrize('date, cpi', CHECK_POINTS)
+def test_get_monthly_cpi(date, cpi):
     df = get_monthly_cpi()
-    assert df.CPI.loc['1991-01-31'] == 1.0620   
-    assert df.CPI.loc['2018-01-31'] == 1.0031 
+    assert df.CPI.loc[date] == cpi
