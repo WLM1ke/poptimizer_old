b5e93ed1d16efd4867aa24e7adb03524e0a7f3a0
WLM1ke <wlmike@gmail.com>
2018-03-12 09:26:42 +0300

    Тесты на 3 сценария - создание, обновление и без обновления данных

--- src/portfolio/getter/test/test_history.py
+++ src/portfolio/getter/test/test_history.py
@@ -1,68 +1,67 @@
+from pathlib import Path
+
 import arrow
 import pandas as pd
 import pytest
 
-from portfolio import download
+from portfolio import download, settings
 from portfolio.getter import history
 from portfolio.getter.history import get_quotes_history, load_quotes_history, df_last_date, validate_last_date
 
 
 @pytest.fixture(scope='class')
-def get_df():
+def new_df(tmpdir_factory):
+    # Этап 1 - инициируется создание DataFrame с нуля во временной директории
+    saved_path = settings.DATA_PATH
+    temp_dir = tmpdir_factory.mktemp('data')
+    settings.DATA_PATH = Path(temp_dir)
+    yield get_quotes_history('MSTT')
+    settings.DATA_PATH = saved_path
+
+
+@pytest.fixture(scope='class')
+def updated_df(new_df):
+    # Этап 2 - иницируется обновление DataFrame путем подмены параметра окончания торгового дня
+    saved_date = history.END_OF_CURRENT_TRADING_DAY
+    history.END_OF_CURRENT_TRADING_DAY = arrow.get().shift(months=1)
+    df2 = get_quotes_history('MSTT')
+    history.END_OF_CURRENT_TRADING_DAY = saved_date
+    return df2
+
+
+@pytest.fixture(scope='class')
+def no_update_df(updated_df):
+    # Этап 3 - повторный вызов без обновления с вовращенным параметром окончания торгового дня
     return get_quotes_history('MSTT')
 
 
+@pytest.fixture(scope='class', params=range(3))
+def df(request, new_df, updated_df, no_update_df):
+    dfs = [new_df, updated_df, no_update_df]
+    return dfs[request.param]
+
+
 class TestGetQuotesHistory:
-    def test_df_type(self, get_df):
-        assert isinstance(get_df, pd.DataFrame)
+    def test_df_type(self, df):
+        assert isinstance(df, pd.DataFrame)
 
-    def test_df_columns_number(self, get_df):
-        assert len(get_df.columns) == 2
+    def test_df_columns_number(self, df):
+        assert len(df.columns) == 2
 
-    def test_df_index_monotonic(self, get_df):
-        assert get_df.index.is_monotonic_increasing
+    def test_df_index_monotonic(self, df):
+        assert df.index.is_monotonic_increasing
 
-    def test_df_is_unique(self, get_df):
-        assert get_df.index.is_unique
+    def test_df_is_unique(self, df):
+        assert df.index.is_unique
 
-    def test_beginning_date(self, get_df):
-        assert get_df.index[0] == pd.to_datetime('2010-11-03')
+    def test_beginning_date(self, df):
+        assert df.index[0] == pd.to_datetime('2010-11-03')
 
-    def test_load_long_data(self, get_df):
-        assert get_df.shape[0] > 100
+    def test_load_long_data(self, df):
+        assert df.shape[0] > 100
 
-    def test_checkpoint(self, get_df):
-        assert get_df.loc['2018-03-09', 'CLOSE'] == 148.8 and get_df.loc['2018-03-09', 'VOLUME'] == 2960
-
-@pytest.fixture(scope='class')
-def update_df():
-    saved_date = history.END_OF_CURRENT_TRADING_DAY
-    history.END_OF_CURRENT_TRADING_DAY = arrow.get().shift(months=1)
-    yield get_quotes_history('MSTT')
-    history.END_OF_CURRENT_TRADING_DAY = saved_date
-
-
-class TestUpdateQuotesHistory:
-    def test_df_type(self, update_df):
-        assert isinstance(update_df, pd.DataFrame)
-
-    def test_df_columns_number(self, update_df):
-        assert len(update_df.columns) == 2
-
-    def test_df_index_monotonic(self, update_df):
-        assert update_df.index.is_monotonic_increasing
-
-    def test_df_is_unique(self, update_df):
-        assert update_df.index.is_unique
-
-    def test_beginning_date(self, update_df):
-        assert update_df.index[0] == pd.to_datetime('2010-11-03')
-
-    def test_load_long_data(self, update_df):
-        assert update_df.shape[0] > 100
-
-    def test_checkpoint(self, update_df):
-        assert update_df.loc['2018-03-09', 'CLOSE'] == 148.8 and update_df.loc['2018-03-09', 'VOLUME'] == 2960
+    def test_checkpoint(self, df):
+        assert df.loc['2018-03-09', 'CLOSE'] == 148.8 and df.loc['2018-03-09', 'VOLUME'] == 2960
 
 
 def test_validate_last_date_error():
