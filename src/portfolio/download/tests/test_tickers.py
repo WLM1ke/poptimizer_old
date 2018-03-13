340f1511ae934af19c28b3cc36c3243159c0093c
WLM1ke <wlmike@gmail.com>
2018-03-13 20:06:07 +0300

    Добавлены тесты для поиска тикеров по регистрационному номеру

---
+++ src/portfolio/download/tests/test_tickers.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ..tickers import get_tickers
+
+check_points = [('1-02-65104-D', 'UPRO EONR OGK4'),
+                ('10301481B', 'SBER SBER03'),
+                ('20301481B', 'SBERP SBERP03'),
+                ('1-02-06556-A', 'PHOR')]
+
+
+@pytest.mark.parametrize("reg_number, expected", check_points)
+def test_get_tickers(reg_number, expected):
+    assert get_tickers(reg_number) == expected
