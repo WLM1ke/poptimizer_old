65394ebab717244353261bb09a438db107291b5c
WLM1ke <wlmike@gmail.com>
2018-03-14 13:31:35 +0300

    Close #31 - Реализовано полное покрытие тестами security_info

--- src/portfolio/getter/tests/test_security_info.py
+++ src/portfolio/getter/tests/test_security_info.py
@@ -15,9 +15,9 @@ def fake_data_path(tmpdir_factory):
     settings.DATA_PATH = saved_path
 
 
-def test_first_time_security_info():
-    df = security_info.get_security_info(['KBTK', 'MOEX'])
-    assert len(df.index) == 2
+def test_first_time_get_last_prices():
+    df = security_info.get_last_prices(['KBTK', 'MOEX'])
+    assert len(df.columns) == 2
     assert len(security_info.load_securities_info().index) == 2
 
 
@@ -42,9 +42,35 @@ def test_not_all_tickers_in_local_data_security_info():
 def test_all_tickers_are_new_time_security_info():
     df = security_info.get_security_info(['SNGSP', 'GAZP'])
     assert len(df.index) == 2
+    assert len(df.columns) == 4
     df_local = security_info.load_securities_info()
     assert len(df_local.index) == 5
     assert df.equals(df_local.loc[['SNGSP', 'GAZP']])
     assert df_local.loc['KBTK', 'SHORTNAME'] == 'КузбТК ао'
     assert df_local.loc['MOEX', 'REGNUMBER'] == '1-05-08443-H'
     assert df.loc['SNGSP', 'LOTSIZE'] == 100
+
+
+def test_local_tickers_get_lots_size():
+    df = security_info.get_lots_size(['SNGSP', 'GAZP'])
+    assert len(df.index) == 1
+    assert len(df.columns) == 2
+    assert df.loc['LOTSIZE', 'SNGSP'] == 100
+    assert df.loc['LOTSIZE', 'GAZP'] == 10
+
+
+def test_not_local_tickers_get_lots_size():
+    df = security_info.get_lots_size(['SNGSP', 'GMKN'])
+    assert len(df.index) == 1
+    assert len(df.columns) == 2
+    assert df.loc['LOTSIZE', 'SNGSP'] == 100
+    assert df.loc['LOTSIZE', 'GMKN'] == 1
+
+
+def test_fake_no_local_data_get_lots_size(monkeypatch):
+    monkeypatch.setattr(security_info, 'DATA_FILE', 'securities_info2.csv')
+    df = security_info.get_lots_size(['SNGSP', 'GMKN'])
+    assert len(df.index) == 1
+    assert len(df.columns) == 2
+    assert df.loc['LOTSIZE', 'SNGSP'] == 100
+    assert df.loc['LOTSIZE', 'GMKN'] == 1
