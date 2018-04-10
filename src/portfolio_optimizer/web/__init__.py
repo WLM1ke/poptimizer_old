a416bce0713a42ba67ef86a62e3be607e85b8dea
WLM1ke <wlmike@gmail.com>
2018-04-10 15:46:45 +0300

    Поправлены тесты

--- src/portfolio_optimizer/web/__init__.py
+++ src/portfolio_optimizer/web/__init__.py
@@ -1,7 +1,7 @@
 """Download and transform data to pandas DataFrames."""
 
 from .web_cpi import cpi
-from .web_dividends import dividends as dividends
+from .web_dividends import dividends
 from .web_history import get_index_history as index_history
 from .web_history import get_quotes_history as quotes_history
 from .web_securities_info import get_securities_info as securities_info
--- src/portfolio_optimizer/web/tests/test_cpi.py
+++ src/portfolio_optimizer/web/tests/test_cpi.py
@@ -1,10 +1,12 @@
 import pytest
 
+from portfolio_optimizer.web.web_cpi import cpi
+
 CHECK_POINTS = [('1991-01-31', 1.0620),
                 ('2018-01-31', 1.0031)]
 
 
-@pytest.mark.parametrize('date, cpi', CHECK_POINTS)
-def test_get_monthly_cpi(date, cpi):
+@pytest.mark.parametrize('date, value', CHECK_POINTS)
+def test_cpi(date, value):
     df = cpi()
-    assert df[date] == cpi
+    assert df[date] == value
