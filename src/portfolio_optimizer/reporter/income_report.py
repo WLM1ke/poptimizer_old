cf81526106a00d68173827e7a6e8656fcf3adaaa
WLM1ke <wlmike@gmail.com>
2018-05-15 13:44:01 +0300

    Добавлены тесты для отчета об доходах

--- src/portfolio_optimizer/reporter/income_report.py
+++ src/portfolio_optimizer/reporter/income_report.py
@@ -1,9 +1,10 @@
 """Расчет дивидендов и дохода за последний год в пересчете на неделю и месяц в постоянных ценах"""
+
 from portfolio_optimizer import local
 from portfolio_optimizer.reporter.reporter import read_data
 
 
-def get_investor_data(investor_name, report_name):
+def get_investor_data(report_name: str, investor_name: str):
     """Формирует DataFrame с вкладами, стоимостью активов и дивидендами инвестора"""
     df = read_data(report_name)
     df = df.iloc[-13:]
@@ -17,7 +18,7 @@ def get_investor_data(investor_name, report_name):
 
 def constant_prices_data(report_name: str, investor_name: str):
     """Переводит данные в постоянные цены"""
-    df = get_investor_data(investor_name, report_name)
+    df = get_investor_data(report_name, investor_name)
     cpi = local.cpi()
     cpi = cpi[-13:]
     cpi = cpi.cumprod()
@@ -27,7 +28,7 @@ def constant_prices_data(report_name: str, investor_name: str):
 
 def rescale_and_format(x, multiplier):
     """Умножает на множитель и форматирует с округлением до тысяч, разделением разрядов и выравниванием вправо"""
-    return f'{round(x * multiplier, -3):,.0f}'.replace(',', ' ').rjust(10)
+    return f'{round(x * multiplier, -3):,.0f}'.replace(',', ' ').rjust(9)
 
 
 def income_report(report_name: str, investor_name: str):
@@ -39,5 +40,5 @@ def income_report(report_name: str, investor_name: str):
     print(f'\n{investor_name}')
     for period, multiplier in time_periods.items():
         print(f'1{period}:',
-              f'Dividends = {rescale_and_format(dividends, multiplier)},',
-              f'Income = {rescale_and_format(income, multiplier)}')
+              f'Real Dividends = {rescale_and_format(dividends, multiplier)},',
+              f'Real Income = {rescale_and_format(income, multiplier)}')
---
+++ src/portfolio_optimizer/reporter/tests/test_income_report.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from shutil import copyfile
+
+import pytest
+
+from portfolio_optimizer.reporter import reporter
+from portfolio_optimizer.reporter.income_report import get_investor_data, constant_prices_data, rescale_and_format, \
+    income_report
+
+
+@pytest.fixture(scope='module', autouse=True)
+def make_fake_data(tmpdir_factory):
+    saved_data_path = reporter.REPORTS_DATA_PATH
+    temp_dir = tmpdir_factory.mktemp('test_income_report')
+    reporter.REPORTS_DATA_PATH = Path(temp_dir) / 'data'
+    reporter.REPORTS_DATA_PATH.mkdir(parents=True)
+    copyfile(Path(__file__).parent / 'data' / 'test.xlsx', reporter.REPORTS_DATA_PATH / 'test.xlsx')
+    yield
+    reporter.REPORTS_DATA_PATH = saved_data_path
+
+
+def test_get_investor_data():
+    df = get_investor_data('test', 'Igor')
+    assert df.shape == (13, 3)
+    assert df.loc['2018-01-19', 'Inflow'] == pytest.approx(-6300)
+    assert df.loc['2018-04-19', 'Value'] == pytest.approx(10094.11382)
+    assert df.loc['2017-06-19', 'Dividends'] == pytest.approx(12.18696831)
+
+
+def test_constant_prices_data():
+    df = constant_prices_data('test', 'Igor')
+    assert df.shape == (13, 3)
+    assert df.loc['2018-01-19', 'Inflow'] == pytest.approx(-6351.166136)
+    assert df.loc['2017-07-19', 'Value'] == pytest.approx(12875.75482)
+    assert df.loc['2017-06-19', 'Dividends'] == pytest.approx(12.38775237)
+
+
+def test_rescale_and_format():
+    assert rescale_and_format(1234567, 1) == '1 235 000'
+    assert rescale_and_format(1234567, 0.1) == '  123 000'
+
+
+def test_income_report(capsys):
+    income_report('test', 'WLMike')
+    captured_string = capsys.readouterr().out
+    assert 'WLMike' in captured_string
+    assert '1Y: Real Dividends =    28 000, Real Income =    85 000' in captured_string
+    assert '1M: Real Dividends =     2 000, Real Income =     7 000' in captured_string
+    assert '1W: Real Dividends =     1 000, Real Income =     2 000' in captured_string
