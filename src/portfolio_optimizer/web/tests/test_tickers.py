83af0f1315b77336cc1ecaa2afbecb0d7047b817
WLM1ke <wlmike@gmail.com>
2018-06-05 10:32:04 +0300

    Дополнены тесты

--- src/portfolio_optimizer/web/tests/test_tickers.py
+++ src/portfolio_optimizer/web/tests/test_tickers.py
@@ -1,8 +1,9 @@
-import pytest
+from urllib import request
+from urllib.error import URLError
 
 from ..web_tickers import reg_number_tickers
 
-check_points = [('1-02-65104-D', ('UPRO', 'EONR', 'OGK4')),
+'''check_points = [('1-02-65104-D', ('UPRO', 'EONR', 'OGK4')),
                 ('10301481B', ('SBER', 'SBER03')),
                 ('20301481B', ('SBERP', 'SBERP03')),
                 ('1-02-06556-A', ('PHOR',))]
@@ -10,4 +11,25 @@ check_points = [('1-02-65104-D', ('UPRO', 'EONR', 'OGK4')),
 
 @pytest.mark.parametrize("reg_number, expected", check_points)
 def test_reg_number_tickers(reg_number, expected):
-    assert reg_number_tickers(reg_number) == expected
+    assert reg_number_tickers(reg_number) == expected'''
+
+
+class FakeUrlopen:
+
+    def __init__(self):
+        self.saved_urlopen = request.urlopen
+        request.urlopen = self
+        self.called = False
+
+    def __call__(self, url):
+        if not self.called:
+            self.called = True
+            request.urlopen = self.saved_urlopen
+            raise URLError(TimeoutError())
+        else:
+            self.saved_urlopen(url)
+
+
+def test_get_json_error():
+    FakeUrlopen()
+    assert reg_number_tickers('1-02-65104-D') == ('UPRO', 'EONR', 'OGK4')
