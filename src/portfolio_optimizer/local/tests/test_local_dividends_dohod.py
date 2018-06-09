a308c7c3803c6bc3e90af2d1e728f8f2a9039b29
WLM1ke <wlmike@gmail.com>
2018-06-09 09:47:51 +0300

    Исправление теста по локальным дивидендам с dohod.ru

--- src/portfolio_optimizer/local/tests/test_local_dividends_dohod.py
+++ src/portfolio_optimizer/local/tests/test_local_dividends_dohod.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-from portfolio_optimizer.local import local_dividends
+from portfolio_optimizer.local.local_dividends_dohod import dividends
 
 
 def test_dividends():
-    df = local_dividends.dividends('GAZP')
+    df = dividends('GAZP')
     assert isinstance(df, pd.Series)
     assert df.name == 'GAZP'
     assert df.index.is_monotonic_increasing
     assert df.index.is_unique
-    assert df.loc['2002-05-13'] == 0.44
+    assert df.loc['2003-02-21'] == 0.4
     assert df.loc['2017-07-20'] == 8.04
