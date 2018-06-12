d01f796278c9ab208fc07f20b5194f5e6b91c4ec
WLM1ke <wlmike@gmail.com>
2018-06-12 09:45:21 +0300

    Функция месячной инфляции до заданной даты

--- src/portfolio_optimizer/local/local_cpi.py
+++ src/portfolio_optimizer/local/local_cpi.py
@@ -30,5 +30,33 @@ def cpi():
     return data.get()
 
 
+def cpi_to_date(last_date: pd.Timestamp):
+    """Месячная инфляция с учетом даты окончания
+
+    Даты, на которую дана месячная инфляция соответствует дню месяца из last_date
+    Если дата после окончания статистики, то она продлевается исходя из последней годовой инфляции
+
+    Parameters
+    ----------
+    last_date
+        Конечная дата в ряде данных месячной инфляции
+    Returns
+    -------
+    pd.Series
+        В строках значения инфляции для каждого месяца
+        Инфляция 1,2% за месяц соответствует 1.012
+    """
+    df = cpi()
+    index = pd.DatetimeIndex(name=df.index.name,
+                             freq='M',
+                             start=df.index[0],
+                             end=last_date + pd.DateOffset(day=31))
+    df = df.reindex(index)
+    df = df.fillna(df.shift(12))
+    index = index.map(pd.DateOffset(day=last_date.day))
+    df.index = index
+    return df
+
+
 if __name__ == '__main__':
-    print(cpi())
+    print(cpi_to_date(pd.Timestamp('2018-07-19')))
