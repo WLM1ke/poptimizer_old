7b50ad064432577c44f0f5d213b565c4bb7a41ea
WLM1ke <wlmike@gmail.com>
2018-08-06 20:42:51 +0300

    Тесты для новой реализации CPI

--- src/local/tests/test_local_cpi.py
+++ src/local/tests/test_local_cpi.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import pytest
 
+import web
 from local import local_cpi
+from local.local_cpi import CPIDataManager, cpi_to_date
 from web.labels import CPI
 
 
@@ -13,3 +16,27 @@ def test_cpi():
     assert df.index[0] == pd.to_datetime('1991-01-31')
     assert df.shape[0] >= 326
     assert df.loc['2018-02-28'] == 1.0021
+
+
+def test_download_all():
+    manager = CPIDataManager()
+    df = manager.download_all()
+    assert df.equals(web.cpi())
+
+
+def test_download_update():
+    manager = CPIDataManager()
+    with pytest.raises(NotImplementedError):
+        manager.download_update()
+
+
+def test_cpi_to_date(monkeypatch):
+    fake_df = local_cpi.cpi()[:'2018-06-30']
+    monkeypatch.setattr(local_cpi, 'cpi', lambda: fake_df)
+    df = cpi_to_date(pd.Timestamp('2018-08-06'))
+    assert len(df) == 332
+    assert df.index[0] == pd.Timestamp('1991-01-06')
+    assert df.index[-1] == pd.Timestamp('2018-08-06')
+    assert df.iat[-3] == pytest.approx(1.0049)
+    assert df.iat[-2] == pytest.approx(1.0007)
+    assert df.iat[-1] == pytest.approx(0.9946)
