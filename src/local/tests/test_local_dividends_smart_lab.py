4052178cf1e467cbd5a72e759adef3e120dd11a3
WLM1ke <wlmike@gmail.com>
2018-08-09 19:17:54 +0300

    Тесты для СмартЛаба

--- src/local/tests/test_local_dividends_smart_lab.py
+++ src/local/tests/test_local_dividends_smart_lab.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import arrow
+import pandas as pd
 import pytest
 
 import settings
 import web
 from local import local_dividends_smart_lab
+from local.local_dividends_smart_lab import SmartLabDataManager
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -16,17 +19,26 @@ def make_temp_dir(tmpdir_factory):
     settings.DATA_PATH = saved_path
 
 
-def test_dividends_smart_lab_first_time():
+def test_dividends_smart_lab():
     df = local_dividends_smart_lab.dividends_smart_lab()
     assert df.equals(web.dividends_smart_lab())
 
 
-def test_need_not_update():
-    manager = local_dividends_smart_lab.SmartLabDataManager()
-    assert not manager._need_update()
+def test_update():
+    manager = SmartLabDataManager()
+    time0 = arrow.now()
+    assert manager.last_update < time0
+    manager.update()
+    assert time0 < manager.last_update
 
 
-def test_need_update_true(monkeypatch):
-    monkeypatch.setattr(local_dividends_smart_lab.SmartLabDataManager, 'days_to_update', 0)
-    manager = local_dividends_smart_lab.SmartLabDataManager()
-    assert manager._need_update()
+def test_download_update():
+    with pytest.raises(NotImplementedError):
+        manager = SmartLabDataManager()
+        manager.download_update()
+
+
+def test_dividends_smart_lab_for_ticker():
+    df = local_dividends_smart_lab.dividends_smart_lab('CHMF')
+    assert isinstance(df, pd.Series)
+    assert df.name == 'CHMF'
