338f086d4e3ff37764240cc44fc243e5ad7520d2
WLM1ke <wlmike@gmail.com>
2018-08-19 09:59:13 +0300

    Упрощена реализация

--- src/ml/current_predictor.py
+++ src/ml/current_predictor.py
@@ -1,24 +1,24 @@
 """Реализация текущего предсказания в формате sklearn"""
-
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics import r2_score
 
 
 class AverageRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, lags=5):
-        self._lags = lags
+    def __init__(self):
+        pass
 
     def get_params(self, deep=True):
-        return {'lags': self._lags}
+        return {}
 
     def set_params(self, **params):
-        self._lags = params['lags']
+        pass
 
     def fit(self, x, y, sample_weight=None):
         return self
 
-    def predict(self, x):
-        return x.values[:, -self._lags:].mean(axis=1)
+    @staticmethod
+    def predict(x):
+        return x.values.mean(axis=1)
 
     def score(self, x, y, sample_weight=None):
         return r2_score(y, self.predict(x), sample_weight)
