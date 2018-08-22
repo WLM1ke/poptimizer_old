d3e1c21d5d677f44f34965fea4d5f81007b91efe
WLM1ke <wlmike@gmail.com>
2018-08-22 13:38:23 +0300

    Добавлена квартальная агрегация и тесты к ней

--- src/utils/aggregation.py
+++ src/utils/aggregation.py
@@ -42,6 +42,44 @@ def yearly_aggregation_func(end_of_year: pd.Timestamp):
     return functools.partial(yearly_aggregator, end_of_year=end_of_year)
 
 
+def quarterly_aggregator(date: pd.Timestamp, end_of_quarter: pd.Timestamp):
+    """Округляет дату вверх числа конца квартала
+
+    Parameters
+    ----------
+    date
+        Дата, для которой необходимо рассчитать квартальную агрегацию
+    end_of_quarter
+        Пример даты окончания квартала - ее число используются для агрегации
+    Returns
+    -------
+    pd.Timestamp
+        Возвращает дату окончания квартала для указанной даты
+    """
+    end_day = end_of_quarter.day
+    months_to_end_of_quarter = (end_of_quarter.month - date.month) % 3
+    if (0, date.day) <= (months_to_end_of_quarter, end_day):
+        return date + pd.DateOffset(months=months_to_end_of_quarter, day=end_day)
+    else:
+        return date + pd.DateOffset(months=3, day=end_day)
+
+
+def quarterly_aggregation_func(end_of_quarter: pd.Timestamp):
+    """Возвращает функцию для квартальной агрегации
+
+    Parameters
+    ----------
+    end_of_quarter
+        Дата число которой считаются окончанием квартала
+    Returns
+    -------
+    function
+        Агрегирующая функция - принимает дату в формате pd.Timestamp в качестве единственного аргумента и возвращает
+        дату окончания квартала для нее
+    """
+    return functools.partial(quarterly_aggregator, end_of_quarter=end_of_quarter)
+
+
 def monthly_aggregator(date: pd.Timestamp, end_of_month: pd.Timestamp):
     """Округляет дату вверх числа конца месяца
 
@@ -63,12 +101,12 @@ def monthly_aggregator(date: pd.Timestamp, end_of_month: pd.Timestamp):
         return date + pd.DateOffset(months=1, day=end_day)
 
 
-def monthly_aggregation_func(end_of_year: pd.Timestamp):
+def monthly_aggregation_func(end_of_month: pd.Timestamp):
     """Возвращает функцию для месячной агрегации
 
     Parameters
     ----------
-    end_of_year
+    end_of_month
         Дата число которой считаются окончанием месяца
     Returns
     -------
@@ -76,4 +114,17 @@ def monthly_aggregation_func(end_of_year: pd.Timestamp):
         Агрегирующая функция - принимает дату в формате pd.Timestamp в качестве единственного аргумента и возвращает
         дату окончания месяца для нее
     """
-    return functools.partial(monthly_aggregator, end_of_month=end_of_year)
+    return functools.partial(monthly_aggregator, end_of_month=end_of_month)
+
+
+if __name__ == '__main__':
+    func = quarterly_aggregation_func(pd.Timestamp('2018-08-20'))
+    print(func(pd.Timestamp('2018-08-18')))
+    print(func(pd.Timestamp('2018-08-20')))
+    print(func(pd.Timestamp('2018-08-21')))
+    print(func(pd.Timestamp('2018-07-21')))
+    print(func(pd.Timestamp('2018-06-21')))
+    print(func(pd.Timestamp('2018-05-21')))
+    print(func(pd.Timestamp('2018-04-21')))
+    print(func(pd.Timestamp('2018-03-21')))
+    print(func(pd.Timestamp('2018-12-21')))
--- src/utils/tests/test_aggregation.py
+++ src/utils/tests/test_aggregation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from utils.aggregation import monthly_aggregator, monthly_aggregation_func
+from utils.aggregation import monthly_aggregator, monthly_aggregation_func, quarterly_aggregator, \
+    quarterly_aggregation_func
 from utils.aggregation import yearly_aggregator, yearly_aggregation_func
 
 
@@ -23,6 +24,28 @@ def test_yearly_aggregation_func():
     assert result(pd.Timestamp('2014-01-31')) == pd.Timestamp('2014-03-11')
     assert result(pd.Timestamp('2012-02-28')) == pd.Timestamp('2012-03-11')
     assert result(pd.Timestamp('2015-09-13')) == pd.Timestamp('2016-03-11')
+
+
+def test_quarterly_aggregator():
+    assert quarterly_aggregator(pd.Timestamp('2018-08-13'), pd.Timestamp('2011-03-11')) == pd.Timestamp('2018-09-11')
+    assert quarterly_aggregator(pd.Timestamp('2014-09-14'), pd.Timestamp('2014-09-13')) == pd.Timestamp('2014-12-13')
+    assert quarterly_aggregator(pd.Timestamp('2017-02-28'), pd.Timestamp('2014-01-31')) == pd.Timestamp('2017-04-30')
+    assert quarterly_aggregator(pd.Timestamp('2017-02-28'), pd.Timestamp('2014-07-31')) == pd.Timestamp('2017-04-30')
+    assert quarterly_aggregator(pd.Timestamp('2011-10-31'), pd.Timestamp('2013-02-28')) == pd.Timestamp('2011-11-28')
+    assert quarterly_aggregator(pd.Timestamp('2011-01-31'), pd.Timestamp('2013-02-28')) == pd.Timestamp('2011-02-28')
+    assert quarterly_aggregator(pd.Timestamp('2011-10-31'), pd.Timestamp('2012-02-29')) == pd.Timestamp('2011-11-29')
+    assert quarterly_aggregator(pd.Timestamp('2011-01-31'), pd.Timestamp('2012-02-29')) == pd.Timestamp('2011-02-28')
+    assert quarterly_aggregator(pd.Timestamp('2012-12-31'), pd.Timestamp('2012-02-29')) == pd.Timestamp('2013-02-28')
+
+
+def test_quarterly_aggregation_func():
+    result = quarterly_aggregation_func(pd.Timestamp('2019-04-11'))
+    assert callable(result)
+    assert result(pd.Timestamp('2018-08-13')) == pd.Timestamp('2018-10-11')
+    assert result(pd.Timestamp('2016-03-11')) == pd.Timestamp('2016-04-11')
+    assert result(pd.Timestamp('2014-01-31')) == pd.Timestamp('2014-04-11')
+    assert result(pd.Timestamp('2012-02-28')) == pd.Timestamp('2012-04-11')
+    assert result(pd.Timestamp('2015-09-13')) == pd.Timestamp('2015-10-11')
 
 
 def test_monthly_aggregator():
