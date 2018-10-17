5725319fe38b5f7cf2e7815758401cbfda3837f1
WLM1ke <wlmike@gmail.com>
2018-10-17 09:01:54 +0300

    Добавлены docstrings

--- src/local/moex/iss_index.py
+++ src/local/moex/iss_index.py
@@ -11,9 +11,14 @@ class IndexDataManager(AbstractDataManager):
         super().__init__(None, INDEX_NAME)
 
     def download_all(self):
+        """Загружает все данные по индексу полной доходности"""
         return moex.index()
 
     def download_update(self):
+        """Загружает новые данные с последнего значения, включительно
+
+        Последнее значение используется для проверки стыковки данных
+        """
         last_date = self.value.index[-1]
         return moex.index(last_date)
 
