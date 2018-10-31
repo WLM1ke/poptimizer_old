393ed130087d7d09b722a9720d465bb3e96b5fca
WLM1ke <wlmike@gmail.com>
2018-10-31 15:47:58 +0300

    Парсинг conomy

--- src/web/dividends/conomy_ru.py
+++ src/web/dividends/conomy_ru.py
@@ -1,5 +1,7 @@
 """Загрузка данных о дивидендах с https://www.conomy.ru/"""
-from bs4 import BeautifulSoup
+import re
+
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,6 +10,9 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support import wait
 
 # Время ожидания загрузки
+from web.dividends.table_parser import AbstractTableParser, DataColumn
+from web.labels import DATE
+
 WAITING_TIME = 10
 
 # Параметры поиска страницы эмитента
@@ -17,6 +22,16 @@ SEARCH_FIELD = '//*[@id="issuer_search"]'
 # Параметры поиска данных по дивидендам
 DIVIDENDS_MENU = '//*[@id="page-wrapper"]/div/nav/ul/li[5]/a'
 DIVIDENDS_TABLE = '//*[@id="page-container"]/div[2]/div/div[1]'
+
+TABLE_INDEX = 1
+
+ORDINARY = 'ORDINARY'
+PREFERRED = 'PREFERRED'
+
+# Параметры парсинга
+NO_VALUE = '-'
+DIV_PATTERN = r'.*\d'
+DATE_PATTERN = r'\d{2}\.\d{2}\.\d{4}'
 
 
 def xpath_await_find(driver, xpath: str, waiting_time: int = WAITING_TIME):
@@ -41,23 +56,53 @@ def load_dividends_table(driver):
     """Выбирает на странице эмитента меню дивиденды и загружает таблицу с ними"""
     element = xpath_await_find(driver, DIVIDENDS_MENU)
     element.click()
+    xpath_await_find(driver, DIVIDENDS_TABLE)
 
 
 def get_html(ticker: str):
-    """Возвращает html код страницы с данными по дивидендам"""
+    """Возвращает html код страницы с данными по дивидендам и ее url"""
     driver_options = options.Options()
     driver_options.headless = True
     with webdriver.Firefox(options=driver_options) as driver:
         load_ticker_page(ticker, driver)
         load_dividends_table(driver)
-        xpath_await_find(driver, DIVIDENDS_TABLE)
-        return driver.page_source
+        return driver.page_source, driver.current_url
+
+
+def div_parser(data: str):
+    if data == NO_VALUE:
+        return 0.0
+    data = re.search(DIV_PATTERN, data).group(0)
+    data = data.replace(',', '.')
+    return float(data)
+
+
+def date_parser(data: str):
+    if data == NO_VALUE:
+        return None
+    date = re.search(DATE_PATTERN, data).group(0)
+    return pd.to_datetime(date, dayfirst=True)
+
+
+class ConomyTableParser(AbstractTableParser):
+    """Парсер таблиц с дивидендами с https://www.conomy.ru/"""
+    TABLE_INDEX = TABLE_INDEX
+    HEADER_TITLES = [[None, None, None, 'Дата закрытия реестра акционеров', None,
+                      'Размер дивидендов\nна одну акцию, руб.'],
+                     [None, None, None, None, 'Под выплату дивидендов', 'АОИ', 'АПИ']]
+    FOOTER_TITLES = []
+    DATA_COLUMNS = [DataColumn(DATE, 5, date_parser),
+                    DataColumn(ORDINARY, 7, div_parser),
+                    DataColumn(PREFERRED, 8, div_parser)]
+
+    def __init__(self, ticker: str):
+        self._ticker = ticker
+
+    def get_html(self):
+        return get_html(self._ticker)
 
 
 if __name__ == '__main__':
     ticker_ = 'AKRN'
-    html = get_html(ticker_)
-    soup = BeautifulSoup(html, 'lxml')
-    for table in soup.find_all('table'):
-        print('*' * 50)
-        print(table)
+    table = ConomyTableParser(ticker_)
+    print(table.df)
--- src/web/dividends/table_parser.py
+++ src/web/dividends/table_parser.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 
 class DataColumn(NamedTuple):
+    """Описание колонки с данными"""
     name: str
     index: int
     parser_func: Callable = pd.to_numeric
@@ -15,6 +16,7 @@ class DataColumn(NamedTuple):
 class AbstractTableParser(ABC):
     """Абстрактный парсер html таблиц"""
     TABLE_INDEX = None
+    # Каждая строка заголовка отдельный массив срок. Если проверка наименования строки не нужно, то None
     HEADER_TITLES = []
     FOOTER_TITLES = []
     DATA_COLUMNS = []
@@ -41,29 +43,37 @@ class AbstractTableParser(ABC):
         soup = bs4.BeautifulSoup(html, 'lxml')
         index = self.TABLE_INDEX
         try:
-            return soup.find_all('table')[index]
+            table = soup.find_all('table')[index]
         except IndexError:
             raise IndexError(f'На странице {url} нет таблицы {index}')
+        table_body = table.find('tbody')
+        if table_body:
+            return table_body
+        return table
 
     @staticmethod
     def _validate_table(rows, titles):
-        """Проверяет корректность заголовков"""
+        """Проверяет корректность наименований"""
         for row, row_titles in zip(rows, titles):
-            cells = row.find_all('th')
+            cells = row.find_all()
             if len(cells) != len(row_titles):
-                raise ValueError('Некорректная длинна строк с наименованиями')
+                raise ValueError(f'Длинна строки с наименованиями {len(cells)} - должна быть {len(row_titles)}')
             for cell, title in zip(cells, row_titles):
-                if (title is not None) and (cell != title):
-                    raise ValueError('Некорректные наименования в таблице')
+                if (title is not None) and (cell.text != title):
+                    raise ValueError(f'Наименования в таблице "{cell.text}" - должно быть "{title}"')
 
     def _parse_row(self, row):
-        cells = row.find_all('td')
-        return [column.parser_func(cells[column.index]) for column in self.DATA_COLUMNS]
+        """Выбирает столбцы с данными и  преобразует значения"""
+        cells = row.find_all()
+        return [column.parser_func(cells[column.index].text) for column in self.DATA_COLUMNS]
 
     def _get_columns_names(self):
+        """Возвращает список с названиями столбцов с данными"""
         return [column.name for column in self.DATA_COLUMNS]
 
+    @property
     def df(self):
+        """DataFrame с таблицей"""
         columns = self._get_columns_names()
-        data = self
+        data = iter(self)
         return pd.DataFrame(data=data, columns=columns)
