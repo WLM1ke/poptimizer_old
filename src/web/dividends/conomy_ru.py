98e9272cc8a805517b80950115f66217b6eef300
WLM1ke <wlmike@gmail.com>
2018-11-04 12:16:27 +0300

    Тест для загрузки данных с conomy.ru

--- src/web/dividends/conomy_ru.py
+++ src/web/dividends/conomy_ru.py
@@ -31,15 +31,8 @@ DIV_PATTERN = r'.*\d'
 DATE_PATTERN = r'\d{2}\.\d{2}\.\d{4}'
 
 
-def div_parser(data: str):
-    if data == NO_VALUE:
-        return 0.0
-    data = re.search(DIV_PATTERN, data).group(0)
-    data = data.replace(',', '.')
-    return float(data)
-
-
 def date_parser(data: str):
+    """Функция парсинга значений в колонке с датами закрытия реестра"""
     if data == NO_VALUE:
         return None
     date = re.search(DATE_PATTERN, data).group(0)
@@ -50,6 +43,16 @@ DATE_COLUMN = parser.DataColumn(5,
                                 {0: 'Дата закрытия реестра акционеров',
                                  1: 'Под выплату дивидендов'},
                                 date_parser)
+
+
+def div_parser(data: str):
+    """Функция парсинга значений в колонке с дивидендами"""
+    if data == NO_VALUE:
+        return 0.0
+    data = re.search(DIV_PATTERN, data).group(0)
+    data = data.replace(',', '.')
+    return float(data)
+
 
 COMMON_TICKER_LENGTH = 4
 COMMON_COLUMN = parser.DataColumn(7,
@@ -63,37 +66,37 @@ PREFERRED_COLUMN = parser.DataColumn(8,
                                      div_parser)
 
 
-def xpath_await_find(driver, xpath: str, waiting_time: int = WAITING_TIME):
+def xpath_await(driver, xpath: str, waiting_time: int = WAITING_TIME):
     """Ищет и возвращает элемент с заданным xpath
 
     При необходимости предварительно ждет загрузки в течении определенного количества секунд
     """
-    load_wait = wait.WebDriverWait(driver, waiting_time)
-    element_wait = expected_conditions.presence_of_element_located((By.XPATH, xpath))
-    element = load_wait.until(element_wait)
+    waiting_driver = wait.WebDriverWait(driver, waiting_time)
+    element_xpath = expected_conditions.presence_of_element_located((By.XPATH, xpath))
+    element = waiting_driver.until(element_xpath)
     return element
 
 
-def load_ticker_page(ticker: str, driver):
-    """Вводит на страницу поиска тикер и переходит на страницу с информацией по эмитенту"""
+def load_ticker_page(driver, ticker: str):
+    """Вводит в поле поиска тикер и переходит на страницу с информацией по эмитенту"""
     driver.get(SEARCH_URL)
-    element = xpath_await_find(driver, SEARCH_FIELD)
+    element = xpath_await(driver, SEARCH_FIELD)
     element.send_keys(ticker, Keys.ENTER)
 
 
 def load_dividends_table(driver):
-    """Выбирает на странице эмитента меню дивиденды и загружает таблицу с ними"""
-    element = xpath_await_find(driver, DIVIDENDS_MENU)
+    """Выбирает на странице эмитента меню дивиденды и дожидается загрузки таблиц с ними"""
+    element = xpath_await(driver, DIVIDENDS_MENU)
     element.click()
-    xpath_await_find(driver, DIVIDENDS_TABLE)
+    xpath_await(driver, DIVIDENDS_TABLE)
 
 
 def get_html(ticker: str):
-    """Возвращает html код страницы с данными по дивидендам"""
+    """Возвращает html-код страницы с данными по дивидендам с сайта https://www.conomy.ru/"""
     driver_options = options.Options()
     driver_options.headless = True
     with webdriver.Firefox(options=driver_options) as driver:
-        load_ticker_page(ticker, driver)
+        load_ticker_page(driver, ticker)
         load_dividends_table(driver)
         return driver.page_source
 
@@ -121,7 +124,8 @@ def dividends_conomy(ticker: str):
         Строки - даты закрытия реестра упорядоченные по возрастанию
         Значения - дивиденды
     """
-    table = parser.HTMLTableParser(get_html(ticker), TABLE_INDEX)
+    html = get_html(ticker)
+    table = parser.HTMLTableParser(html, TABLE_INDEX)
     columns = [DATE_COLUMN]
     if is_common(ticker):
         columns.append(COMMON_COLUMN)
---
+++ src/web/dividends/tests/test_conomy_ru.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox import options
+
+from web.dividends import conomy_ru
+
+
+def test_date_parser():
+    assert conomy_ru.date_parser('-') is None
+    assert conomy_ru.date_parser('30.11.2018 (рек.)') == pd.Timestamp('2018-11-30')
+    assert conomy_ru.date_parser('19.07.2017') == pd.Timestamp('2017-07-19')
+
+
+def test_div_parser():
+    assert conomy_ru.div_parser('2.23') == 2.23
+    assert conomy_ru.div_parser('30,4') == 30.4
+    assert conomy_ru.div_parser('4') == 4
+    assert conomy_ru.div_parser('66.8 (рек.)') == 66.8
+    assert conomy_ru.div_parser('78,9 (прогноз)') == 78.9
+    assert conomy_ru.div_parser('-') == 0.0
+
+
+def test_xpath_await():
+    driver_options = options.Options()
+    driver_options.headless = True
+    with webdriver.Firefox(options=driver_options) as driver:
+        driver.get(conomy_ru.SEARCH_URL)
+        element = conomy_ru.xpath_await(driver, conomy_ru.SEARCH_FIELD)
+        assert element.get_attribute('placeholder') == 'Поиск эмитентов по названию или тикеру'
+
+
+def test_xpath_await_no_element():
+    driver_options = options.Options()
+    driver_options.headless = True
+    with webdriver.Firefox(options=driver_options) as driver, pytest.raises(TimeoutException) as error:
+        driver.get(conomy_ru.SEARCH_URL)
+        conomy_ru.xpath_await(driver, conomy_ru.SEARCH_FIELD + '/div', 1)
+    assert error.type == TimeoutException
+
+
+def test_load_ticker_page():
+    driver_options = options.Options()
+    driver_options.headless = True
+    with webdriver.Firefox(options=driver_options) as driver:
+        conomy_ru.load_ticker_page(driver, 'AKRN')
+        conomy_ru.xpath_await(driver, conomy_ru.DIVIDENDS_MENU)
+        assert driver.current_url == 'https://www.conomy.ru/emitent/akron'
+
+
+def test_load_dividends_table():
+    driver_options = options.Options()
+    driver_options.headless = True
+    with webdriver.Firefox(options=driver_options) as driver:
+        driver.get('https://www.conomy.ru/emitent/akron')
+        conomy_ru.load_dividends_table(driver)
+        assert driver.current_url == 'https://www.conomy.ru/emitent/akron/akrn-div'
+
+
+def test_get_html():
+    assert 'размер выплачиваемых ОАО «Акрон» дивидендов должен' in conomy_ru.get_html('AKRN')
+
+
+def test_is_common():
+    assert conomy_ru.is_common('CHMF')
+    assert not conomy_ru.is_common('SNGSP')
+    with pytest.raises(ValueError) as error:
+        conomy_ru.is_common('TANGO')
+    assert str(error.value) == 'Некорректный тикер TANGO'
+
+
+def test_dividends_conomy_common():
+    df = conomy_ru.dividends_conomy('SBER')
+    assert isinstance(df, pd.Series)
+    assert df.size == 9
+    assert df.index[0] == pd.Timestamp('2010-04-16')
+    assert df['2011-04-15'] == 0.92
+
+
+def test_dividends_conomy_preferred():
+    df = conomy_ru.dividends_conomy('SBERP')
+    assert isinstance(df, pd.Series)
+    assert df.size == 9
+    assert df.index[0] == pd.Timestamp('2010-04-16')
+    assert df['2012-04-12'] == 2.59
