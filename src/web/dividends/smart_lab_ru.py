24a02c70a84d5f219e02ef5e7d39866c76e88130
WLM1ke <wlmike@gmail.com>
2018-11-05 17:02:03 +0300

    Данные со smart-lab с помощью универсального парсера таблиц

--- src/web/dividends/smart_lab_ru.py
+++ src/web/dividends/smart_lab_ru.py
@@ -1,69 +1,26 @@
 """Загружает данные по предстоящим дивидендам с https://www.smart-lab.ru"""
-import re
-
-import pandas as pd
-from bs4 import BeautifulSoup
-
-from web.dividends.dohod_ru import get_html_table, RowParserDohod
+from web.dividends import dohod_ru, parser
 from web.labels import TICKER, DATE, DIVIDENDS
 
 URL = 'https://smart-lab.ru/dividends/index/order_by_yield/desc/'
-# Номер таблицы с дивидендами в документе
 TABLE_INDEX = 2
-# Позиции и наименования ключевых столбцов
-TH_TICKER = 'Тикер'
-TH_DATE = 'дата отсечки'
-TH_VALUE = 'дивиденд,руб'
-TICKER_COLUMN = 1
-DATE_COLUMN = 4
-VALUE_COLUMN = 7
-DATE_PATTERN = r'\d{2}\.\d{2}\.\d{4}'
+HEADER_SIZE = 1
+FOOTER_SIZE = 1
 
+TICKER_COLUMN = parser.DataColumn(1,
+                                  {0: 'Тикер',
+                                   -1: '\n+добавить дивиденды\nИстория выплаченных дивидендов\n'},
+                                  lambda x: x)
 
-class RowParserSmartLab(RowParserDohod):
-    """Выбирает столбцы с тикером, датой закрытия реестра и дивидендами"""
+DATE_COLUMN = parser.DataColumn(4,
+                                {0: 'дата отсечки',
+                                 -1: '\n+добавить дивиденды\nИстория выплаченных дивидендов\n'},
+                                parser.date_parser)
 
-    ticker_column = TICKER_COLUMN
-    date_column = DATE_COLUMN
-    value_column = VALUE_COLUMN
-
-    @property
-    def ticker(self):
-        """Тикер"""
-        return self.columns[self.ticker_column]
-
-
-def validate_table_header(header: BeautifulSoup):
-    """Проверка количества столбцов и наименований с тикером, датой закрытия и дивидендами"""
-    columns_count = len(header.find_all('th'))
-    if columns_count != 10:
-        raise ValueError('Некорректное количество столбцов в заголовке таблицы дивидендов.')
-    cells = RowParserSmartLab(header, True)
-    if cells.ticker != TH_TICKER or cells.date != TH_DATE or cells.value != TH_VALUE:
-        raise ValueError('Некорректные заголовки таблицы дивидендов.')
-
-
-def validate_table_footer(footer: BeautifulSoup):
-    """В последнем ряду должна быть одна ячейка"""
-    columns_count = len(footer.find_all('td'))
-    if columns_count != 1:
-        raise ValueError('Некорректная последняя строка таблицы дивидендов.')
-
-
-def parse_table_rows(table: BeautifulSoup):
-    """Строки с прогнозом имеют class = dividend_approved"""
-    rows = table.find_all(name='tr')
-    validate_table_header(rows[0])
-    validate_table_footer(rows[-1])
-    for row in rows[1:-1]:
-        if 'class' in row.attrs and 'dividend_approved' in row['class']:
-            cells = RowParserSmartLab(row)
-            yield (cells.ticker,
-                   pd.to_datetime(re.search(DATE_PATTERN, cells.date).group(0), dayfirst=True),
-                   pd.to_numeric(cells.value.replace(',', '.')))
-        else:
-            # Если появятся ячейки без класса утвержденных дивидендов, то надо разобраться с обработкой
-            raise ValueError('Не утвержденные дивиденды')
+DIVIDENDS_COLUMN = parser.DataColumn(7,
+                                     {0: 'дивиденд,руб',
+                                      -1: '\n+добавить дивиденды\nИстория выплаченных дивидендов\n'},
+                                     parser.div_parser)
 
 
 def dividends_smart_lab():
@@ -76,10 +33,11 @@ def dividends_smart_lab():
         Строки - тикеры
         столбцы - даты закрытия и дивиденды
     """
-    table = get_html_table(URL, TABLE_INDEX)
-    parsed_rows = parse_table_rows(table)
-    df = pd.DataFrame(data=parsed_rows,
-                      columns=[TICKER, DATE, DIVIDENDS])
+    html = dohod_ru.get_html(URL)
+    table = parser.HTMLTableParser(html, TABLE_INDEX)
+    columns = [TICKER_COLUMN, DATE_COLUMN, DIVIDENDS_COLUMN]
+    df = table.make_df(columns, HEADER_SIZE, FOOTER_SIZE)
+    df.columns = [TICKER, DATE, DIVIDENDS]
     return df.set_index(DATE)
 
 
