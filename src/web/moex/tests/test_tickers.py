7110d7102100b45891ffd2767e168f7f459fe8cf
WLM1ke <wlmike@gmail.com>
2018-11-16 16:05:57 +0300

    Исправлены тесты

--- src/web/moex/tests/test_tickers.py
+++ src/web/moex/tests/test_tickers.py
@@ -1,9 +1,8 @@
-from urllib import request
-from urllib.error import URLError
+import pytest
 
 from web.moex.iss_tickers import reg_number_tickers
 
-'''check_points = [('1-02-65104-D', ('UPRO', 'EONR', 'OGK4')),
+check_points = [('1-02-65104-D', ('UPRO', 'EONR', 'OGK4')),
                 ('10301481B', ('SBER', 'SBER03')),
                 ('20301481B', ('SBERP', 'SBERP03')),
                 ('1-02-06556-A', ('PHOR',))]
@@ -11,25 +10,4 @@ from web.moex.iss_tickers import reg_number_tickers
 
 @pytest.mark.parametrize("reg_number, expected", check_points)
 def test_reg_number_tickers(reg_number, expected):
-    assert reg_number_tickers(reg_number) == expected'''
-
-
-class FakeUrlopen:
-
-    def __init__(self):
-        self.saved_urlopen = request.urlopen
-        request.urlopen = self
-        self.called = False
-
-    def __call__(self, url):
-        if not self.called:
-            self.called = True
-            request.urlopen = self.saved_urlopen
-            raise URLError(TimeoutError())
-        else:
-            self.saved_urlopen(url)
-
-
-def test_get_json_error():
-    FakeUrlopen()
-    assert reg_number_tickers('1-02-65104-D') == ('UPRO', 'EONR', 'OGK4')
+    assert reg_number_tickers(reg_number) == expected
